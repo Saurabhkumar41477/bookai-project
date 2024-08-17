73dfac6c1de87927194802aa97802274812612e5
Saurabh kumar <112726977+Saurabhkumar41477@users.noreply.github.com>
2024-08-17 23:38:47 +0530

    Add files via upload

---
+++ core.js
@@ -0,0 +1,6 @@
+document.getElementById('theme-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
+function selectPlan(plan) {
+    alert(`You have selected the ${plan} plan.`);
+}
